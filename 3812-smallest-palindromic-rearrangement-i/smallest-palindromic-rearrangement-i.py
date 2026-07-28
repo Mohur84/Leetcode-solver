a8de06bcbd37021da618ae0e36d7788b7ad8521c
Mohur Datta <mohur.23bai11091@vitbhopal.ac.in>
2026-07-28 23:52:01 +0530

    Time: 387 ms (64.71%) | Memory: 13.4 MB (85.29%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,20 @@
+class Solution:
+    def smallestPalindrome(self, s):
+        cnt = [0] * 26
+        for c in s:
+            cnt[ord(c) - ord('a')] += 1
+        
+        half = []
+        mid = ""
+        for i in range(26):
+            half.append(chr(ord('a') + i) * (cnt[i] // 2))
+            if cnt[i] % 2 == 1:
+                mid = chr(ord('a') + i)
+        
+        half_str = "".join(half)
+        return half_str + mid + half_str[::-1]
+
+sol = Solution()
+print(sol.smallestPalindrome("babab"))
+print(sol.smallestPalindrome("daccad"))
+print(sol.smallestPalindrome("z"))
