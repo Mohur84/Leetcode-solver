5c1f3b4b0c20095ba96e4473090f8270d6cef85e
Mohur Datta <mohardatta@gmail.com>
2026-09-12 19:04:01 +0530

    Time: 85 ms (74.13%) | Memory: 22.5 MB (83.31%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,35 @@
+from collections import deque
+class Codec:
+    def serialize(self, root):
+        if not root:
+            return ""
+        result = []
+        q = deque([root])
+        while q:
+            node = q.popleft()
+
+            if node is None:
+                result.append("#")
+            else:
+                result.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right)
+        return ",".join(result)
+    def deserialize(self, data):
+        if not data:
+            return None
+        nodes = data.split(",")
+        root = TreeNode(int(nodes[0]))
+        q = deque([root])
+        i = 1
+        while q:
+            current = q.popleft()
+            if nodes[i] != "#":
+                current.left = TreeNode(int(nodes[i]))
+                q.append(current.left)
+            i += 1
+            if nodes[i] != "#":
+                current.right = TreeNode(int(nodes[i]))
+                q.append(current.right)
+            i += 1
+        return root
