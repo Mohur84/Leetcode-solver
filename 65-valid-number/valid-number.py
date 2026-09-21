086c96b9d13162e1f33c281430e95f742258f857
Mohur Datta <mohardatta@gmail.com>
2026-09-21 21:41:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (40.13%) - LeetSync

---
+++ 65-valid-number/valid-number.py
@@ -0,0 +1,28 @@
+
+
+class Solution:
+    def isNumber(self, s):
+        s = s.strip()
+        seenDigit = False
+        seenDot = False
+        seenExp = False
+        digitAfterExp = True
+        for i, ch in enumerate(s):
+            if ch.isdigit():
+                seenDigit = True
+                digitAfterExp = True
+            elif ch in ['+', '-']:
+                if i > 0 and s[i - 1] not in ['e', 'E']:
+                    return False
+            elif ch == '.':
+                if seenDot or seenExp:
+                    return False
+                seenDot = True
+            elif ch in ['e', 'E']:
+                if seenExp or not seenDigit:
+                    return False
+                seenExp = True
+                digitAfterExp = False
+            else:
+                return False
+        return seenDigit and digitAfterExp
