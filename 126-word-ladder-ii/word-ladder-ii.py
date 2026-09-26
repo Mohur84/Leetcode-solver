72292695924fa60cda8cdd8f2b1b0b0e3ddf0e36
Mohur Datta <mohardatta@gmail.com>
2026-09-26 13:22:08 +0530

    Time: 55 ms (8.47%) | Memory: 19.7 MB (49.30%) - LeetSync

---
+++ 126-word-ladder-ii/word-ladder-ii.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+class Solution:
+    def findLadders(self, beginWord, endWord, wordList):
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return []
+
+        # BFS to compute shortest distance from beginWord
+        distance = {beginWord: 0}
+        q = deque([beginWord])
+
+        while q:
+            word = q.popleft()
+            step = distance[word]
+
+            for i in range(len(word)):
+                for c in "abcdefghijklmnopqrstuvwxyz":
+                    newWord = word[:i] + c + word[i+1:]
+
+                    if newWord in wordSet and newWord not in distance:
+                        distance[newWord] = step + 1
+                        q.append(newWord)
+
+        if endWord not in distance:
+            return []
+
+        ans = []
+        path = [endWord]
+
+        # DFS from endWord back to beginWord
+        def dfs(word):
+            if word == beginWord:
+                ans.append(path[::-1])
+                return
+
+            step = distance[word]
+
+            for i in range(len(word)):
+                for c in "abcdefghijklmnopqrstuvwxyz":
+                    prevWord = word[:i] + c + word[i+1:]
+
+                    if prevWord in distance and distance[prevWord] == step - 1:
+                        path.append(prevWord)
+                        dfs(prevWord)
+                        path.pop()
+
+        dfs(endWord)
+        return ans
