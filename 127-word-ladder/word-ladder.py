5964fbe3ea615354b318d90672a0b85f48fb7247
Mohur Datta <mohardatta@gmail.com>
2026-09-26 16:59:30 +0530

    Time: 281 ms (55.69%) | Memory: 20.7 MB (41.58%) - LeetSync

---
+++ 127-word-ladder/word-ladder.py
@@ -0,0 +1,18 @@
+class Solution:
+    def ladderLength(self, beginWord, endWord, wordList):
+        wordSet=set(wordList)
+        if endWord not in wordSet:
+            return 0
+        q=deque()
+        q.append((beginWord, 1))
+        while q:
+            word, steps=q.popleft()
+            if word==endWord:
+                return steps
+            for i in range(len(word)):
+                for c in "abcdefghijklmnopqrstuvwxyz":
+                    newWord=word[:i]+c+word[i+1:]
+                    if newWord in wordSet:
+                        wordSet.remove(newWord)
+                        q.append((newWord, steps+1))
+        return 0
