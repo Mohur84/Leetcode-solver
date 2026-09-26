7e0893024a4fe44fc7655ee00fa5a1800a146741
Mohur Datta <mohardatta@gmail.com>
2026-09-26 17:46:03 +0530

    Time: 43 ms (60.20%) | Memory: 33.5 MB (86.72%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partition(self, s):
+        ans=[]
+        path=[]
+        def isPalindrome(left, right):
+            while left<right:
+                if s[left]!=s[right]:
+                    return False
+                left+=1
+                right-=1
+            return True
+        def backtrack(start):
+            if start==len(s):
+                ans.append(path[:])
+                return
+            for end in range(start, len(s)):
+                if isPalindrome(start, end):
+                    path.append(s[start:end+1])
+                    backtrack(end+1)
+                    path.pop()
+        backtrack(0)
+        return ans
